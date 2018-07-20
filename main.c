e7d1a593c6d957c0847667dea95ea1841a236065
Trevor Parsh <tparsh@gmail.com>
2018-07-20 07:36:46 -0400

    added two files

---
+++ main.c
@@ -0,0 +1,15 @@
+
+void main(void)
+{
+	return 0;
+}
+
+static void f1(void)
+{
+	return 0;
+}
+
+static void f2(void)
+{
+	return 0;
+}
---
+++ otherfile.c
@@ -0,0 +1,9 @@
+
+#include <stdint.h>
+
+void printstuff(void)
+{
+	uint8_t printInt = 0;
+	
+	printf("I eat %d", printInt);
+}
